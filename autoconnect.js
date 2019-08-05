9eaaa8324b56f1b7cd585b4590d5137520b360bc
Dmitry Pestov <pestov.ua@gmail.com>
2019-08-06 01:14:00 +0300

    ...

--- autoconnect.js
+++ autoconnect.js
@@ -1,41 +1,47 @@
 'use strict';
 console.clear();
-
 const connectBtn = '.search-result__action-button';
-const approveBtn = 'button.ml1';
-const pause = 1000; // pause in ms
+const approveBtn = ['BUTTON', 'ml1'];
+let pause = 3000; // pause in ms
 
-let body = document.querySelector('body');
-body.scrollIntoView(false);
-
-setTimeout(worker(), 1000);
-
-function worker(){
-  let connect = document.querySelectorAll(connectBtn);
-  for(let i=0; i < connect.length; i++) {
-    addHR(i, approve);
-  }
-
-  // get popup with approve and recommendation add message in connect
-  function addHR(i, callback){
-    setTimeout(()=>{
-      connect[i].click();
-        console.log(connect[i]);
-        console.log('clicked');
-      callback();
-    }, pause);
-  }
-
-  function insertMessage() {
-  //  to be continued
-  }
-
-  //press button approve
-  function approve() {
-    const approve = document.querySelector(approveBtn);
-    setTimeout(()=>{
-      approve.click();
-      console.log('closed');
-    }, pause);
-  }
+function start(callback){
+  let body = document.querySelector('body');
+  body.scrollIntoView(false);
+  setTimeout(() => callback(), pause);
 }
+
+function worker() {
+  document.querySelectorAll(connectBtn).forEach((item)=>clearButton(item));
+  let connect = document.querySelector(connectBtn);
+  if(connect === null) return console.log('Finished');
+  let a = document.addEventListener('DOMNodeInserted', function (event) {
+    let el = event.target;
+    if (el.tagName === 'DIV' && el.classList.contains('ip-fuse-limit-alert')) return console.log('Limitited connection stopped');
+    if (el.tagName === approveBtn[0] && el.classList.contains(approveBtn[1])){
+      console.log('Add listener');
+      let b = el.addEventListener('click', function () {
+        console.log('Approved');
+        connect.remove();
+        // a.removeEventListener('DOMNodeInserted');
+        // b.removeEventListener('click');
+        setTimeout(() => worker(), 1000);
+      });
+      approve(el);
+    }
+  });
+  connect.click();
+}
+
+// press button approve
+function approve(el) {
+  setTimeout(function () {
+    el.click();
+    console.log('closed');
+  }, pause);
+}
+// delete buttons already sended connect
+function clearButton(el){
+  if(el.disabled) el.remove();
+}
+
+start(worker);
